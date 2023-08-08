f63860f25fb1ae027fa9ed51a74a9cb334863318
iurii_gusev <gusevyuv@gmail.com>
2023-08-08 14:53:09 -0400

    HW_OOP_@ was made

---
+++ src/EngineVehicle.java
@@ -0,0 +1,14 @@
+public abstract class EngineVehicle extends Vehicle {
+    public EngineVehicle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    public void service() {
+        super.service();
+        checkEngine();
+    }
+}
